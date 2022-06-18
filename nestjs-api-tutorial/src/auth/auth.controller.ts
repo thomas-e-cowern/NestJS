93b5e7ba65c7350e203d054423e64068e5e1580a
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-06-18 15:45:05 -0400

    added login and signup function to auth.service

--- nestjs-api-tutorial/src/auth/auth.controller.ts
+++ nestjs-api-tutorial/src/auth/auth.controller.ts
@@ -1,3 +1,6 @@
 import { Controller } from '@nestjs/common';
+import { AuthService } from './auth.service';
 @Controller({})
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+}
