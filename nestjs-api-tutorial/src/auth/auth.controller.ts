57f817adb0d46be2f450c3ac362d528b25ea8b06
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-06-20 15:17:16 -0400

    updated auth index and auth controler

--- nestjs-api-tutorial/src/auth/auth.controller.ts
+++ nestjs-api-tutorial/src/auth/auth.controller.ts
@@ -1,11 +1,12 @@
 import { Body, Controller, Post, Req } from '@nestjs/common';
 import { Request } from 'express';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
   @Post('signup')
-  signup(@Body() dto: any) {
+  signup(@Body() dto: AuthDto) {
     console.log(dto);
     return this.authService.signup();
   }
