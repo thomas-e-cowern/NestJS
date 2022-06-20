4e7b09314eccadc77930e60c7292610c8e5497e9
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-06-20 19:47:00 -0400

    added hashing to auth service

--- nestjs-api-tutorial/src/auth/auth.service.ts
+++ nestjs-api-tutorial/src/auth/auth.service.ts
@@ -2,17 +2,23 @@ import { Injectable, Req } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2'
+import { EMPTY_OBSERVER } from 'rxjs/internal/Subscriber';
 
 @Injectable({})
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signup(dto: AuthDto) {
+  async signup(dto: AuthDto) {
     // Generate the password
-
+    const hash = await argon.hash(dto.password)
     // Save user
-
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash
+      }
+    })
     // Return saved user
-    return { msg: 'I have signed up' };
+    return user;
   }
 
   signin() {
