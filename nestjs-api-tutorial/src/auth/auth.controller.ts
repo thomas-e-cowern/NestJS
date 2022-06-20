ad38f39642c2a0b8eadb168e5bc97fdd84fc5057
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-06-20 15:36:52 -0400

    updated auth controller with pipes

--- nestjs-api-tutorial/src/auth/auth.controller.ts
+++ nestjs-api-tutorial/src/auth/auth.controller.ts
@@ -1,13 +1,17 @@
-import { Body, Controller, Post, Req } from '@nestjs/common';
-import { Request } from 'express';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
   @Post('signup')
-  signup(@Body() dto: AuthDto) {
-    console.log(dto);
+  signup(@Body('email') email: string, @Body('password') password: string) {
+    console.log({
+      email,
+      typeOfEmail: typeof email,
+      password,
+      typeOfPassword: typeof password
+    });
     return this.authService.signup();
   }
 
@@ -15,4 +19,3 @@ export class AuthController {
   signin() {
     return this.authService.signin();
   }
-}
