5d017dd530aa82787a43892e5b651d6fb604e93b
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-06-20 16:33:53 -0400

    installed argon and imported into dto

--- nestjs-api-tutorial/src/auth/auth.service.ts
+++ nestjs-api-tutorial/src/auth/auth.service.ts
@@ -1,11 +1,17 @@
 import { Injectable, Req } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
+import * as argon from 'argon2'
 
 @Injectable({})
 export class AuthService {
   constructor(private prisma: PrismaService) {}
   signup(dto: AuthDto) {
+    // Generate the password
+
+    // Save user
+
+    // Return saved user
     return { msg: 'I have signed up' };
   }
 
