4c4b8cb73cb15d383969ac5de8b5e77b843c99ff
Thomas Cowern <thomas.e.cowern@gmail.com>
2022-06-22 14:37:00 -0400

    updated jwt strategy with prisma

--- nestjs-api-tutorial/src/auth/strategy/jwt.strategy.ts
+++ nestjs-api-tutorial/src/auth/strategy/jwt.strategy.ts
@@ -2,10 +2,11 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
-  constructor(config: ConfigService) {
+  constructor(config: ConfigService, prisma: PrismaService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
